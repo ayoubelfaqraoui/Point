3c0f36ba76a22a4835dcc36f436b9af899f9698a
Acer <Acer@DESKTOP-PQOLN1N>
2022-11-28 09:44:39 +0100

    test

--- Point/Program.cs
+++ Point/Program.cs
@@ -10,6 +10,24 @@ namespace Point
     {
         static void Main(string[] args)
         {
+            Point p = new Point(1,2);
+            p.setX(10);
+            p.setY(6);
+
+            Point p2 = new Point(3,4);
+            p2.setX(11);
+            p2.setY(17);
+            p.Distance(p2);
+            Console.WriteLine(p.Distance(p2));
+            Console.WriteLine(p2.Distance(p));
+            p.getX();
+            p.getY();
+            p2.getX();
+            p2.getY();
+            Console.WriteLine(p2.getX());
+            Console.WriteLine(p2.getY());
+            Console.WriteLine(p2.getX());
+            Console.WriteLine(p2.getY());
 
         }
     }
